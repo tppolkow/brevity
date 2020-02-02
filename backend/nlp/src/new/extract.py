a0fd497a34ec4119a9d614c950c57c761cd3dd20
Syed Moiz <syed.m.moiz@gmail.com>
2020-02-02 12:40:15 -0500

    WIP: Improve old nlp implementation

--- backend/nlp/src/new/extract.py
+++ backend/nlp/src/new/extract.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 class Extract:
+    text = ''
     sents = list()
     paras = list()
     summary = list()
@@ -12,8 +13,35 @@ class Extract:
     para_count = 0
     sim_mat = np.zeros((0, 0))
 
-    def tokenize_text(self):
-        pass
+    def read_text(self, path_to_text):
+        with open(path_to_text, 'r') as file:
+            self.text = file.read().replace('\n', '')
+
+    def extract_sents(self):
+        raw_paras = self.text.split('\n')
+        print(raw_paras)
+        print(len(raw_paras))
+        for para in raw_paras:
+            self.para_count += 1
+            sentences = para.split('.')
+            for sent in sentences:
+                sent_instance = Sentence(self.sent_count, sent, len(sent),
+                                         self.para_count)
+                self.sents.append(sent_instance)
+                self.sent_count += 1
+
+    def group_sents_into_paras(self):
+        para_num = 0
+        para = Paragraph(0)
+        for i in range(0, self.sent_count):
+            if self.sents[i].paragraph_number == para_num:
+                pass
+            else:
+                self.paras.append(para)
+                para_num += 1
+                para = Paragraph(para_num)
+            para.sentences.append(self.sents[i])
+        self.paras.append(para)
 
     def get_comm_word_count(self, sentence1, sentence2):
         sent1_words = sentence1.split()
@@ -34,5 +62,20 @@ class Extract:
                 else:
                     self.sim_mat[i][j] = self.sim_mat[j][i]
 
+    def print_sentences(self):
+        for sent in self.sents:
+            print('Sentence #: {}'.format(sent.number))
+            print('Value: {}'.format(sent.value))
+            print('Length: {}'.format(sent.length))
+            print('Word Count: {}'.format(sent.word_count))
+            print('Paragraph #: {}'.format(sent.paragraph_number))
+            print('\n')
+
     def print_summary(self):
         pass
+
+
+extract = Extract()
+extract.read_text('/home/syedmoiz/Documents/brevity/backend/nlp/data/in2.txt')
+extract.extract_sents()
+extract.print_sentences()
