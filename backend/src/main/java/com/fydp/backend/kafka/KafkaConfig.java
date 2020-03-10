834fc979ee1a91b5fbbdc93d51c55b49ac48c17b
Howard Tsai <cttsai@edu.uwaterloo.ca>
2020-03-10 18:23:01 -0400

    Update for frontend and fix backend

--- backend/src/main/java/com/fydp/backend/kafka/KafkaConfig.java
+++ backend/src/main/java/com/fydp/backend/kafka/KafkaConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
@@ -68,7 +69,7 @@ public class KafkaConfig {
         buildDefaults(properties, hostPorts);
 
         properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, hostPorts.stream().collect(Collectors.joining(",")));
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
 
